2f71539b83fa0ab555324f402ea48fa1b600ee2f
林岱慶 <ching.lin@xsgames.com>
2024-03-01 14:14:43 +0800

    Updates

---
+++ assets/slogan-66748b8c.js
@@ -0,0 +1,1 @@
+const o="/election/favicon.png",n="/election/images/slogan.png";export{o as _,n as a};
--- assets/slogan-dedef056.js
+++
@@ -1,1 +0,0 @@
-const o=""+new URL("../favicon.png",import.meta.url).href,r=""+new URL("../images/slogan.png",import.meta.url).href;export{o as _,r as a};
